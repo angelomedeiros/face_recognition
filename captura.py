171f536e1fce058e0c58fb82ac6cf629a05d2a8d
Angelo Medeiros Nóbrega <angelo.medeiros@linkle.io>
2020-02-23 20:03:38 -0300

    Implementing photo capture for training

--- captura.py
+++ captura.py
@@ -2,17 +2,30 @@ import cv2
 
 classificador = cv2.CascadeClassifier("haarcascade-frontalface-default.xml")
 camera = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+amostra = 1
+numeroAmostras = 25
+id = input("Digite seu identificador: ")
+largura, altura = 220, 220
+print("Capturando as faces...")
 
 while (True):
     conectado, imagem = camera.read()
     imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
-    facesDetectadas = classificador.detectMultiScale(imagemCinza, scaleFactor=1.5, minSize=(100,100))
+    facesDetectadas = classificador.detectMultiScale(imagemCinza, scaleFactor=1.5, minSize=(100, 100))
 
     for (x, y, l, a) in facesDetectadas:
         cv2.rectangle(imagem, (x, y), (x+l, y+a), (0, 0, 255), 2)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            imageFace = cv2.resize(imagemCinza[y:y + a, x:x + l], (largura, altura))
+            cv2.imwrite("fotos/pessoa." + str(id) + "." + str(amostra) + ".jpg", imageFace)
+            print("[foto " + str(amostra) + " capturada com sucesso]")
+            amostra += 1
 
     cv2.imshow("Face", imagem)
     cv2.waitKey(1)
+    if amostra >= numeroAmostras + 1:
+        break
 
+print("Faces capturadas com sucesso")
 camera.release()
 cv2.destroyAllWindows()
