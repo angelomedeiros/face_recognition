91f124963735907c8a85d528f1149139065b11db
Angelo Medeiros Nóbrega <angelo.medeiros@linkle.io>
2020-02-23 20:19:22 -0300

    Eye area recognition

--- captura.py
+++ captura.py
@@ -1,6 +1,8 @@
 import cv2
+import numpy
 
 classificador = cv2.CascadeClassifier("haarcascade-frontalface-default.xml")
+classificadorOlho = cv2.CascadeClassifier("haarcascade-eye.xml")
 camera = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 amostra = 1
 numeroAmostras = 25
@@ -15,11 +17,19 @@ while (True):
 
     for (x, y, l, a) in facesDetectadas:
         cv2.rectangle(imagem, (x, y), (x+l, y+a), (0, 0, 255), 2)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            imageFace = cv2.resize(imagemCinza[y:y + a, x:x + l], (largura, altura))
-            cv2.imwrite("fotos/pessoa." + str(id) + "." + str(amostra) + ".jpg", imageFace)
-            print("[foto " + str(amostra) + " capturada com sucesso]")
-            amostra += 1
+        regiao = imagem[y:y + a, x: x + l]
+        regiaoCinzaOlho = cv2.cvtColor(regiao, cv2.COLOR_BGR2GRAY)
+        olhosDetectados = classificadorOlho.detectMultiScale(regiaoCinzaOlho)
+
+        for (ox, oy, ol, oa) in olhosDetectados:
+            cv2.rectangle(regiao, (ox, oy), (ox + ol, oy + oa), (0, 255, 0), 2)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                if numpy.average(imagemCinza) > 110:
+                    imageFace = cv2.resize(imagemCinza[y:y + a, x:x + l], (largura, altura))
+                    cv2.imwrite("fotos/pessoa." + str(id) + "." + str(amostra) + ".jpg", imageFace)
+                    print("[foto " + str(amostra) + " capturada com sucesso]")
+                    amostra += 1
 
     cv2.imshow("Face", imagem)
     cv2.waitKey(1)
